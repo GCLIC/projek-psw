8e64550ac3c859a46497b0f03d75ae9cfc599e76
Jayden <denjay813@gmail.com>
2026-06-02 16:19:08 +0700

    Add DB_PORT support

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ const mailer = nodemailer.createTransport({
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'trimas_db'
